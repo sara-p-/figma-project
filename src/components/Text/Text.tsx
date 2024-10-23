b7beaccd216066a02030469f933846d634b883ed
sara-p- <sarapitt88@gmail.com>
2024-10-23 19:35:47 -0400

    starting the build out the markup

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,13 @@
+import './Text.css'
+
+function Text() {
+  return (
+    <div className='text-box'>
+      <div className='text-wrapper'>
+        <span className='text'>Hello Figma</span>
+      </div>
+    </div>
+  )
+}
+
+export default Text
